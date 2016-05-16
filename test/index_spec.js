0030a25d72389a79b45c00ea903684ee83702590
Eric McKay <eric@andcode.co>
2016-05-16 18:06:08 +0000

    initial commit

--- test/index_spec.js
+++ test/index_spec.js
@@ -1,10 +1,1 @@
 import { expect } from 'chai';
-
-import { add } from '../src/index.js';
-
-describe('index.js', () => {
-    it('adds numbers', () => {
-        expect(add(1,2)).to.eq(3);
-    });
-});
- 
